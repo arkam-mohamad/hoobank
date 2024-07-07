57284c295a653884085f3d7039c17b669e9b19b6
arkam-mohamad <125960377+arkam-mohamad@users.noreply.github.com>
2024-07-07 16:46:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+import airbnb from "./airbnb.svg";
+import bill from "./bill.svg";
+import binance from "./binance.svg";
+import card from "./card.svg";
+import coinbase from "./coinbase.svg";
+import dropbox from "./dropbox.svg";
+import logo from "./logo.svg";
+import quotes from "./quotes.svg";
+import robot from "./robot.svg";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import people01 from "./people01.svg";
+import people02 from "./people02.svg";
+import people03 from "./people03.svg";
+
+export {
+  airbnb,
+  bill,
+  binance,
+  card,
+  coinbase,
+  dropbox,
+  logo,
+  quotes,
+  robot,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03,
+};
